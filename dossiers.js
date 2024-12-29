e290d6657703a358581b649bb1d079adbf2c7c6d
AJdisco <kalumlucas@live.co.uk>
2024-12-29 19:36:52 +0000

    Update dossiers.js

--- dossiers.js
+++ dossiers.js
@@ -3,14 +3,31 @@ document.addEventListener('DOMContentLoaded', () => {
     function fetchDossierFiles() {
         // Simulated example: Fetching the list of files
         // In a real scenario, you might fetch from a server or local directory
-        const files = ['The lawbreaker.txt', 'The Obstinate.txt', 'The Epitath.txt', 'The Illusion.txt', 'The Baker.txt', 'The Future.txt', 'The Protectorate.txt', 'The Misdirected.txt', 'The Iconoclast.txt', 'The Sinnerman.txt', 'The Dessert.txt', 'The Tiebreaker.txt'];
-
+   const files = [
+            { name: 'The lawbreaker.txt', url: 'https://ajdisco.github.io/lawbreaker' },
+            { name: 'The Obstinate.txt', url: 'https://ajdisco.github.io/obstinate' },
+            { name: 'The Epitath.txt', url: 'https://ajdisco.github.io/epitaph' },
+            { name: 'The Illusion.txt', url: 'https://ajdisco.github.io/illusion' },
+            { name: 'The Baker.txt', url: 'https://ajdisco.github.io/baker' },
+            { name: 'The Future.txt', url: 'https://ajdisco.github.io/future' },
+            { name: 'The Protectorate.txt', url: 'https://ajdisco.github.io/protectorate' },
+            { name: 'The Misdirected.txt', url: 'https://ajdisco.github.io/misdirected' },
+            { name: 'The Iconoclast.txt', url: 'https://ajdisco.github.io/iconoclast' },
+            { name: 'The Sinnerman.txt', url: 'https://ajdisco.github.io/sinnerman' },
+            { name: 'The Dessert.txt', url: 'https://ajdisco.github.io/dessert' },
+            { name: 'The Tiebreaker.txt', url: 'https://ajdisco.github.io/tiebreaker' }
+        ];
         const dossierList = document.querySelector('.dossier-list');
         const ul = document.createElement('ul');
 
         files.forEach(file => {
             const li = document.createElement('li');
-            li.textContent = file;
+            const a = document.createElement('a');
+            
+            a.textContent = file.name;
+            a.href = file.url;
+            
+            li.appendChild(a);
             ul.appendChild(li);
         });
 
